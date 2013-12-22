1788e8359186fee3fda188b6934dbd98399f9217
Ben LeVeque <ben.leveque@gmail.com>
2013-12-22 15:46:32 -0800

    added node modules

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		stylus: {
+			compile: {
+				options: {
+					compress: false
+				},
+				files: {
+					// 'LADS/css/StartPage.css': 'LADS/css/StartPage.styl',
+					// 'LADS/css/Artmode.css': 'LADS/css/Artmode.styl',
+					// 'LADS/css/NewCatalog.css': 'LADS/css/NewCatalog.styl',
+					// 'LADS/css/VideoPlayer.css': 'LADS/css/VideoPlayer.styl',
+					// 'LADS/css/TourPlayer.css': 'LADS/css/TourPlayer.styl'
+				}
+			}
+		},
+		jade: {
+			compile: {
+				options: {
+					pretty: true
+				},
+				files: {
+					// 'LADS/html/StartPage.html': 'LADS/html/StartPage.jade',
+					// 'LADS/html/Artmode.html': 'LADS/html/Artmode.jade',
+					// 'LADS/html/NewCatalog.html': 'LADS/html/NewCatalog.jade',
+					// 'LADS/html/VideoPlayer.html': 'LADS/html/VideoPlayer.jade',
+					// 'LADS/html/TourPlayer.html': 'LADS/html/TourPlayer.jade'
+				}
+			}
+		},
+		watch: {
+			files: ['LADS/html/*.jade', 'LADS/css/*.styl'],
+			tasks: ['stylus', 'jade']
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-stylus');
+	grunt.loadNpmTasks('grunt-contrib-jade');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['jade', 'stylus']);
+}
