db32d7382c9bdc31016e8d82b66388ab7b21e3de
Ben LeVeque <ben.leveque@gmail.com>
2014-01-21 18:18:21 -0500

    added TAG.js and concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,6 +39,7 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-stylus');
 	grunt.loadNpmTasks('grunt-contrib-jade');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', ['jade', 'stylus']);
