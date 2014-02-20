4ff21d76b53a6f8b97871d6a203ee39d48aa4177
Ben LeVeque <ben.leveque@gmail.com>
2014-02-19 21:30:04 -0500

    some rin debugging

--- LADS/TAG.js
+++ LADS/TAG.js
@@ -45002,16 +45002,6 @@ LADS.Util.makeNamespace("LADS.TESTS");
         }
 
 
-        var artmodeRoot= $(document.createElement('div')).attr('id', 'sideBar');
-        artmodeRoot.css({
-            'font-size': w/9.6 + '%', // so font-size percentages for descendents work well
-            //height: h + "px",
-            //left: l + "px",
-            //'max-width': w + "px",
-            //'max-height': h + "px",
-           // width: w + "px"
-        });	
-
 
 
         // debugger;
