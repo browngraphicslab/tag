f6d7e52a18a180dd4161a54823f982ad14fb1b98
Ben LeVeque <ben.leveque@gmail.com>
2014-03-19 10:49:22 -0400

    removed yuidocs from .gitignore

---
+++ yuidocs/api.js
@@ -0,0 +1,10 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "LADS.Layout.Artmode",
+        "LADS.Layout.InternetFailurePage.js"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
