9e9de65e3be19c00ab23a96fb180b022a8f48fbd
Dan Zhang <dan_zhang@brown.edu>
2014-04-14 03:30:33 -0400

    fixes #59
    REALLY fixes #59

--- LADS/js/RIN/web/lib/rin-experiences-1.0.js
+++ LADS/js/RIN/web/lib/rin-experiences-1.0.js
@@ -2784,11 +2784,11 @@ window.rin = window.rin || {};
                     var keyframeDuration = nextKeyframeData.offset - keyframeData.offset;
                     var animation = new rin.internal.DoubleAnimation(keyframeDuration, currentKeyframeVolume, nextKeyframeVolume);
                     currentKeyframeVolume = animation.getValueAt(interpolationOffset);
-                    this._audio.volume = _computeEffectiveVolume(currentKeyframeVolume);
+                    this._audio.volume = this._computeEffectiveVolume(currentKeyframeVolume);
                     this._animateVolume(keyframeDuration - interpolationOffset, nextKeyframeVolume);
                 }
                 else {
-                    this._audio.volume = _computeEffectiveVolume(currentKeyframeVolume);
+                    this._audio.volume = this._computeEffectiveVolume(currentKeyframeVolume);
                 }
             }
         },
