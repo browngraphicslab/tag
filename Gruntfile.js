eeb9051d169edb5423959ab8b366b2841f21535d
Ben LeVeque <ben.leveque@gmail.com>
2014-07-21 15:00:28 -0400

    deprecated files, modified gruntfile to update a few script paths to tagcore

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,82 +4,82 @@ module.exports = function(grunt) {
 			options: {
 				separator: '\n;\n',
 				banner: 'var TAG_GLOBAL = function(tagInput) { \
-					        \n    var tagPath           = tagInput.path, \
-					        \n        containerId       = tagInput.containerId, \
-					        \n        ip                = tagInput.serverIp, \
-					        \n        allowServerChange = tagInput.allowServerChange, \
+					        \n    var tagPath            = tagInput.path, \
+					        \n        containerId        = tagInput.containerId, \
+					        \n        ip                 = tagInput.serverIp, \
+					        \n        allowServerChange  = tagInput.allowServerChange, \
 					        \n 		  allowAuthoringMode = tagInput.allowAuthoringMode, \
-					        \n        idleDuration      = tagInput.idleDuration, \
-					        \n        currentPage       = {}, // name and obj properties \
-					        \n        INPUT_TOUR_ID     = tagInput.tourId, // to load to a tour \
+					        \n        idleDuration       = tagInput.idleDuration, \
+					        \n        currentPage        = {}, // name and obj properties \
+					        \n        INPUT_TOUR_ID      = tagInput.tourId, // to load to a tour \
 					        \n        idleTimer; \n\n',
 				footer: '};'
 			},
 			dist: {
 				src: [
-					'TAG/js/jQueryUI/js/jquery-1.7.1.js',
-					'TAG/js/jQueryUI/js/jquery-ui-1.8.16.custom.min.js',
-					'TAG/js/jQueryUI/js/jquery.available.min.js',
-					'TAG/js/jQueryUI/js/jquery.fittext.js',
-					'TAG/js/jQueryUI/js/jquery.autoSize.js',
-					'TAG/js/jQueryUI/js/jquery.numeric.js',
+					'tagcore/js/jQueryUI/js/jquery-1.7.1.js',                // TAGCORE
+					'tagcore/js/jQueryUI/js/jquery-ui-1.8.16.custom.min.js', // TAGCORE
+					'tagcore/js/jQueryUI/js/jquery.available.min.js',        // TAGCORE
+					'tagcore/js/jQueryUI/js/jquery.fittext.js',              // TAGCORE
+					'tagcore/js/jQueryUI/js/jquery.autoSize.js',             // TAGCORE
+					'tagcore/js/jQueryUI/js/jquery.numeric.js',              // TAGCORE
 
-					'TAG/js/seadragon/src/Seadragon.Core.js',
-					'TAG/js/seadragon/src/Seadragon.Config.js',
-					'TAG/js/seadragon/src/Seadragon.Strings.js',
-					'TAG/js/seadragon/src/Seadragon.Debug.js',
-					'TAG/js/seadragon/src/Seadragon.Profiler.js',
-					'TAG/js/seadragon/src/Seadragon.Point.js',
-					'TAG/js/seadragon/src/Seadragon.Rect.js',
-					'TAG/js/seadragon/src/Seadragon.Spring.js',
-					'TAG/js/seadragon/src/Seadragon.Utils.js',
-					'TAG/js/seadragon/src/Seadragon.MouseTracker.js',
-					'TAG/js/seadragon/src/Seadragon.EventManager.js',
-					'TAG/js/seadragon/src/Seadragon.ImageLoader.js',
-					'TAG/js/seadragon/src/Seadragon.Buttons.js',
-					'TAG/js/seadragon/src/Seadragon.TileSource.js',
-					'TAG/js/seadragon/src/Seadragon.DisplayRect.js',
-					'TAG/js/seadragon/src/Seadragon.DeepZoom.js',
-					'TAG/js/seadragon/src/Seadragon.Viewport.js',
-					'TAG/js/seadragon/src/Seadragon.Drawer.js',
-					'TAG/js/seadragon/src/Seadragon.Viewer.js',
+					'tagcore/js/seadragon/src/Seadragon.Core.js',            // TAGCORE
+					'tagcore/js/seadragon/src/Seadragon.Config.js',          // TAGCORE
+					'tagcore/js/seadragon/src/Seadragon.Strings.js',         // TAGCORE
+					'tagcore/js/seadragon/src/Seadragon.Debug.js',           // TAGCORE
+					'tagcore/js/seadragon/src/Seadragon.Profiler.js',        // TAGCORE
+					'tagcore/js/seadragon/src/Seadragon.Point.js',           // TAGCORE
+					'tagcore/js/seadragon/src/Seadragon.Rect.js',            // TAGCORE
+					'tagcore/js/seadragon/src/Seadragon.Spring.js',          // TAGCORE
+					'tagcore/js/seadragon/src/Seadragon.Utils.js',           // TAGCORE
+					'tagcore/js/seadragon/src/Seadragon.MouseTracker.js',    // TAGCORE
+					'tagcore/js/seadragon/src/Seadragon.EventManager.js',    // TAGCORE
+					'tagcore/js/seadragon/src/Seadragon.ImageLoader.js',     // TAGCORE
+					'tagcore/js/seadragon/src/Seadragon.Buttons.js',         // TAGCORE
+					'tagcore/js/seadragon/src/Seadragon.TileSource.js',      // TAGCORE
+					'tagcore/js/seadragon/src/Seadragon.DisplayRect.js',     // TAGCORE
+					'tagcore/js/seadragon/src/Seadragon.DeepZoom.js',        // TAGCORE
+					'tagcore/js/seadragon/src/Seadragon.Viewport.js',        // TAGCORE
+					'tagcore/js/seadragon/src/Seadragon.Drawer.js',          // TAGCORE
+					'tagcore/js/seadragon/src/Seadragon.Viewer.js',
 
-					'TAG/js/RIN/web/lib/knockout-2.2.1.js',
-					'TAG/js/utils/CryptoJS/rollups/sha1.js',
-					'TAG/js/utils/CryptoJS/rollups/sha224.js',
-					'TAG/js/utils/CryptoJS/rollups/sha256.js',
-					'TAG/js/utils/CryptoJS/rollups/sha384.js',
-					'TAG/js/utils/CryptoJS/rollups/sha512.js',
-					'TAG/js/utils/jquery.getScrollbarWidth.js',
-					'TAG/js/utils/jquery.throttle-debounce.js',
-					'TAG/js/utils/jquery-css-transform.js',
-					'TAG/js/utils/jquery-animate-css-transform.js',
-					'TAG/js/utils/jquery.xml2json.js',
-					'TAG/js/utils/json2xml.js',
-					'TAG/js/utils/jquery.hammer.js',
-					'TAG/js/utils/hammer.js',
-					'TAG/js/utils/avltree.js',
-					'TAG/js/utils/avlnode.js',
-					'TAG/js/utils/binaryheap.js',
-					'TAG/js/utils/dataholder.js',
-					'TAG/js/utils/doubleLinkedList.js',
-					'TAG/js/utils/hashtable.js',
-					'TAG/js/d3/d3.v2.js',
-					'tagcore/js/TAG/util/TAG.Util.js', // TAGCORE
-					'TAG/js/html2canvas/html2canvas.js',
-					'TAG/js/utils/jquery.livequery.js',
-					'TAG/js/Autolinker.js-master/dist/Autolinker.js',
+					'tagcore/js/RIN/web/lib/knockout-2.2.1.js',              // TAGCORE
+					'tagcore/js/utils/CryptoJS/rollups/sha1.js',             // TAGCORE
+					'tagcore/js/utils/CryptoJS/rollups/sha224.js',           // TAGCORE
+					'tagcore/js/utils/CryptoJS/rollups/sha256.js',           // TAGCORE
+					'tagcore/js/utils/CryptoJS/rollups/sha384.js',           // TAGCORE
+					'tagcore/js/utils/CryptoJS/rollups/sha512.js',           // TAGCORE
+					'tagcore/js/utils/jquery.getScrollbarWidth.js',          // TAGCORE
+					'tagcore/js/utils/jquery.throttle-debounce.js',          // TAGCORE
+					'tagcore/js/utils/jquery-css-transform.js',              // TAGCORE
+					'tagcore/js/utils/jquery-animate-css-transform.js',      // TAGCORE
+					'tagcore/js/utils/jquery.xml2json.js',                   // TAGCORE
+					'tagcore/js/utils/json2xml.js',                          // TAGCORE
+					'tagcore/js/utils/jquery.hammer.js',                     // TAGCORE
+					'tagcore/js/utils/hammer.js',                            // TAGCORE
+					'tagcore/js/utils/avltree.js',                           // TAGCORE
+					'tagcore/js/utils/avlnode.js',                           // TAGCORE
+					'tagcore/js/utils/binaryheap.js',                        // TAGCORE
+					'tagcore/js/utils/dataholder.js',                        // TAGCORE
+					'tagcore/js/utils/doubleLinkedList.js',                  // TAGCORE
+					'tagcore/js/utils/hashtable.js',                         // TAGCORE
+					'tagcore/js/d3/d3.v2.js',                                // TAGCORE
+					'tagcore/js/TAG/util/TAG.Util.js',                       // TAGCORE
+					'tagcore/js/html2canvas/html2canvas.js',                 // TAGCORE
+					'tagcore/js/utils/jquery.livequery.js',                  // TAGCORE
+					'tagcore/js/Autolinker.js-master/dist/Autolinker.js',    // TAGCORE
 
 					'TAG/js/TAG/tourauthoring/TAG.TourAuthoring.Constants.js',
-					'TAG/js/TAG/util/TAG.Util.Constants.js',
-					'TAG/js/TAG/util/TAG.Util.Splitscreen.js',
-					'TAG/js/TAG/util/TAG.Util.IdleTimer.js',
-					'tagcore/js/TAG/worktop/Worktop.Database.js',  // TAGCORE
-					'tagcore/js/TAG/worktop/Worktop.Doq.js',       // TAGCORE
-					'tagcore/js/TAG/worktop/TAG.Worktop.Database.js', // TAGCORE
+					'tagcore/js/TAG/util/TAG.Util.Constants.js',             // TAGCORE
+					'tagcore/js/TAG/util/TAG.Util.Splitscreen.js',           // TAGCORE
+					'tagcore/js/TAG/util/TAG.Util.IdleTimer.js',             // TAGCORE
+					'tagcore/js/TAG/worktop/Worktop.Database.js',            // TAGCORE
+					'tagcore/js/TAG/worktop/Worktop.Doq.js',                 // TAGCORE
+					'tagcore/js/TAG/worktop/TAG.Worktop.Database.js',        // TAGCORE
 					'TAG/js/TAG/artmode/TAG.AnnotatedImage.js',
-					'TAG/js/TAG/auth/TAG.Auth.js',
-					'tagcore/js/TAG/layout/TAG.Layout.StartPage.js', // TAGCORE
+					'tagcore/js/TAG/auth/TAG.Auth.js',                       // TAGCORE
+					'tagcore/js/TAG/layout/TAG.Layout.StartPage.js',         // TAGCORE
 					'TAG/js/TAG/layout/TAG.Layout.ArtworkViewer.js',
 					'TAG/js/TAG/layout/TAG.Layout.CollectionsPage.js',
 					'TAG/js/TAG/layout/TAG.Layout.InternetFailurePage.js',
@@ -114,15 +114,13 @@ module.exports = function(grunt) {
 					'TAG/js/TAG/tourauthoring/TAG.TourAuthoring.Track.js',
 					'TAG/js/TAG/tourauthoring/TAG.TourAuthoring.VideoTrack.js',
 
-
-
 					'TAG/js/TAG/authoring/TAG.Authoring.SettingsView.js',
 					'TAG/js/TAG/authoring/TAG.Authoring.ButtonGroup.js',
 					'TAG/js/TAG/authoring/TAG.Authoring.FileUploader.js',
 					'TAG/js/TAG/authoring/jscolor/jscolor.js',
 
-					'TAG/js/popcorn.min.js',
-					'TAG/js/popcorn.capture.js',
+					'tagcore/js/popcorn.min.js',                               // TAGCORE
+					'tagcore/js/popcorn.capture.js',                           // TAGCORE
 
 					'TAG/telemetry/telemetry.js',
 					
@@ -182,61 +180,61 @@ module.exports = function(grunt) {
 		},
 		watch: {
 			files: [
-				'TAG/html/*.jade',
-				'TAG/css/*.styl',
+				'tagcore/html/*.jade',
+				'tagcore/css/*.styl',
 
-				'TAG/js/jQueryUI/js/jquery-1.7.1.js',
-				'TAG/js/jQueryUI/js/jquery-ui-1.8.16.custom.min.js',
-				'TAG/js/jQueryUI/js/jquery.available.min.js',
-				'TAG/js/jQueryUI/js/jquery.fittext.js',
-				'TAG/js/jQueryUI/js/jquery.autoSize.js',
-				'TAG/js/jQueryUI/js/jquery.numeric.js',
+				'tagcore/js/jQueryUI/js/jquery-1.7.1.js',
+				'tagcore/js/jQueryUI/js/jquery-ui-1.8.16.custom.min.js',
+				'tagcore/js/jQueryUI/js/jquery.available.min.js',
+				'tagcore/js/jQueryUI/js/jquery.fittext.js',
+				'tagcore/js/jQueryUI/js/jquery.autoSize.js',
+				'tagcore/js/jQueryUI/js/jquery.numeric.js',
 
-				'TAG/js/seadragon/src/Seadragon.Core.js',
-				'TAG/js/seadragon/src/Seadragon.Config.js',
-				'TAG/js/seadragon/src/Seadragon.Strings.js',
-				'TAG/js/seadragon/src/Seadragon.Debug.js',
-				'TAG/js/seadragon/src/Seadragon.Profiler.js',
-				'TAG/js/seadragon/src/Seadragon.Point.js',
-				'TAG/js/seadragon/src/Seadragon.Rect.js',
-				'TAG/js/seadragon/src/Seadragon.Spring.js',
-				'TAG/js/seadragon/src/Seadragon.Utils.js',
-				'TAG/js/seadragon/src/Seadragon.MouseTracker.js',
-				'TAG/js/seadragon/src/Seadragon.EventManager.js',
-				'TAG/js/seadragon/src/Seadragon.ImageLoader.js',
-				'TAG/js/seadragon/src/Seadragon.Buttons.js',
-				'TAG/js/seadragon/src/Seadragon.TileSource.js',
-				'TAG/js/seadragon/src/Seadragon.DisplayRect.js',
-				'TAG/js/seadragon/src/Seadragon.DeepZoom.js',
-				'TAG/js/seadragon/src/Seadragon.Viewport.js',
-				'TAG/js/seadragon/src/Seadragon.Drawer.js',
-				'TAG/js/seadragon/src/Seadragon.Viewer.js',
+				'tagcore/js/seadragon/src/Seadragon.Core.js',
+				'tagcore/js/seadragon/src/Seadragon.Config.js',
+				'tagcore/js/seadragon/src/Seadragon.Strings.js',
+				'tagcore/js/seadragon/src/Seadragon.Debug.js',
+				'tagcore/js/seadragon/src/Seadragon.Profiler.js',
+				'tagcore/js/seadragon/src/Seadragon.Point.js',
+				'tagcore/js/seadragon/src/Seadragon.Rect.js',
+				'tagcore/js/seadragon/src/Seadragon.Spring.js',
+				'tagcore/js/seadragon/src/Seadragon.Utils.js',
+				'tagcore/js/seadragon/src/Seadragon.MouseTracker.js',
+				'tagcore/js/seadragon/src/Seadragon.EventManager.js',
+				'tagcore/js/seadragon/src/Seadragon.ImageLoader.js',
+				'tagcore/js/seadragon/src/Seadragon.Buttons.js',
+				'tagcore/js/seadragon/src/Seadragon.TileSource.js',
+				'tagcore/js/seadragon/src/Seadragon.DisplayRect.js',
+				'tagcore/js/seadragon/src/Seadragon.DeepZoom.js',
+				'tagcore/js/seadragon/src/Seadragon.Viewport.js',
+				'tagcore/js/seadragon/src/Seadragon.Drawer.js',
+				'tagcore/js/seadragon/src/Seadragon.Viewer.js',
 
-				'TAG/js/RIN/web/lib/knockout-2.2.1.js',
-				'TAG/js/utils/CryptoJS/rollups/sha1.js',
-				'TAG/js/utils/CryptoJS/rollups/sha224.js',
-				'TAG/js/utils/CryptoJS/rollups/sha256.js',
-				'TAG/js/utils/CryptoJS/rollups/sha384.js',
-				'TAG/js/utils/CryptoJS/rollups/sha512.js',
-				'TAG/js/utils/jquery.getScrollbarWidth.js',
-				'TAG/js/utils/jquery.throttle-debounce.js',
-				'TAG/js/utils/jquery-css-transform.js',
-				'TAG/js/utils/jquery-animate-css-transform.js',
-				'TAG/js/utils/jquery.xml2json.js',
-				'TAG/js/utils/json2xml.js',
-				'TAG/js/utils/jquery.hammer.js',
-				'TAG/js/utils/hammer.js',
-				'TAG/js/utils/avltree.js',
-				'TAG/js/utils/avlnode.js',
-				'TAG/js/utils/binaryheap.js',
-				'TAG/js/utils/dataholder.js',
-				'TAG/js/utils/doubleLinkedList.js',
-				'TAG/js/utils/hashtable.js',
-				'TAG/js/d3/d3.v2.js',
+				'tagcore/js/RIN/web/lib/knockout-2.2.1.js',
+				'tagcore/js/utils/CryptoJS/rollups/sha1.js',
+				'tagcore/js/utils/CryptoJS/rollups/sha224.js',
+				'tagcore/js/utils/CryptoJS/rollups/sha256.js',
+				'tagcore/js/utils/CryptoJS/rollups/sha384.js',
+				'tagcore/js/utils/CryptoJS/rollups/sha512.js',
+				'tagcore/js/utils/jquery.getScrollbarWidth.js',
+				'tagcore/js/utils/jquery.throttle-debounce.js',
+				'tagcore/js/utils/jquery-css-transform.js',
+				'tagcore/js/utils/jquery-animate-css-transform.js',
+				'tagcore/js/utils/jquery.xml2json.js',
+				'tagcore/js/utils/json2xml.js',
+				'tagcore/js/utils/jquery.hammer.js',
+				'tagcore/js/utils/hammer.js',
+				'tagcore/js/utils/avltree.js',
+				'tagcore/js/utils/avlnode.js',
+				'tagcore/js/utils/binaryheap.js',
+				'tagcore/js/utils/dataholder.js',
+				'tagcore/js/utils/doubleLinkedList.js',
+				'tagcore/js/utils/hashtable.js',
+				'tagcore/js/d3/d3.v2.js',
 				'tagcore/js/TAG/util/TAG.Util.js', // tagcore now
-				'TAG/js/html2canvas/html2canvas.js',
-				'TAG/js/utils/jquery.livequery.js',
-				'TAG/js/Autolinker.js-master/dist/Autolinker.js',
+				'tagcore/js/html2canvas/html2canvas.js',
+				'tagcore/js/utils/jquery.livequery.js',
+				'tagcore/js/Autolinker.js-master/dist/Autolinker.js',
 				
 				'TAG/js/TAG/tourauthoring/TAG.TourAuthoring.Constants.js',
 				'TAG/js/TAG/tourauthoring/TAG.TourAuthoring.TimeManager.js',
@@ -265,14 +263,14 @@ module.exports = function(grunt) {
 
 
 
-				'TAG/js/TAG/util/TAG.Util.Constants.js',
-				'TAG/js/TAG/util/TAG.Util.Splitscreen.js',
-				'TAG/js/TAG/util/TAG.Util.IdleTimer.js',
+				'tagcore/js/TAG/util/TAG.Util.Constants.js',
+				'tagcore/js/TAG/util/TAG.Util.Splitscreen.js',
+				'tagcore/js/TAG/util/TAG.Util.IdleTimer.js',
 				'tagcore/js/TAG/worktop/Worktop.Database.js', // TAGCORE
 				'tagcore/js/TAG/worktop/Worktop.Doq.js',      // TAGCORE    
 				'tagcore/js/TAG/worktop/TAG.Worktop.Database.js', // TAGCORE
 				'TAG/js/TAG/artmode/TAG.AnnotatedImage.js',
-				'TAG/js/TAG/auth/TAG.Auth.js',
+				'tagcore/js/TAG/auth/TAG.Auth.js',
 				
 				'tagcore/js/TAG/layout/TAG.Layout.StartPage.js', // TAGCORE
 				'TAG/js/TAG/layout/TAG.Layout.ArtworkViewer.js',
